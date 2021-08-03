9f0c4974bdc7c44b6dab8d590d560ac2b50f7f0f
Lars Steen <lats@aarhus.dk>
2021-08-03 14:41:29 +0200

    Upgrading the dotenv library to be compatible with the forloeb module

--- load.environment.php
+++ load.environment.php
@@ -6,15 +6,9 @@
  */
 
 use Dotenv\Dotenv;
-use Dotenv\Exception\InvalidPathException;
 
 /**
  * Load any .env file. See /.env.example.
  */
-$dotenv = new Dotenv(__DIR__);
-try {
-  $dotenv->load();
-}
-catch (InvalidPathException $e) {
-  // Do nothing. Production environments rarely use .env files.
-}
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
