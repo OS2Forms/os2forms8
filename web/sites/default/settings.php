242b597fcc1d9a1ebfec5924cbdeae8c333d7a17
andriyun <andriy.yun@gmail.com>
2021-08-13 13:10:04 +0200

    Removed mistakenly added code

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -756,16 +756,3 @@ $settings['entity_update_backup'] = TRUE;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
-
-$databases['default']['default'] = array (
-  'database' => 'default',
-  'username' => 'root',
-  'password' => 'root',
-  'prefix' => '',
-  'host' => 'db',
-  'port' => '',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'sadad1232123';
-
