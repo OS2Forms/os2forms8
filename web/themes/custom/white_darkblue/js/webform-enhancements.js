d49ccaad62f80f2311d45d3430fed5433b629bb0
Simon L. Lange <sl@bellcom.dk>
2023-10-31 08:56:00 +0000

    Webform enhancements fix

--- web/themes/custom/white_darkblue/js/webform-enhancements.js
+++ web/themes/custom/white_darkblue/js/webform-enhancements.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-  $("label.option input[type='radio']").change(function() {
-    // Remove the active class from all labels
-    $("label.option").removeClass('active-label');
+(function($) {
+  $(document).ready(function() {
+    $("label.option input[type='radio']").change(function() {
+      // Remove the active class from all labels
+      $("label.option").removeClass('active-label');
 
-    if ($(this).prop('checked')) {
-      // Add the active class to the parent label of the checked radio
-      $(this).parent('label.option').addClass('active-label');
-    }
+      if ($(this).prop('checked')) {
+        // Add the active class to the parent label of the checked radio
+        $(this).parent('label.option').addClass('active-label');
+      }
+    });
   });
-});
+})(jQuery);
