b3974039ba32b3d7b8f4834d56a4f01dcb3a6efa
Simon L. Lange <sl@bellcom.dk>
2023-10-31 08:39:40 +0000

    Added active classes on radio input labels

---
+++ web/themes/custom/white_darkblue/js/webform-enhancements.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("label.option input[type='radio']").change(function() {
+    // Remove the active class from all labels
+    $("label.option").removeClass('active-label');
+
+    if ($(this).prop('checked')) {
+      // Add the active class to the parent label of the checked radio
+      $(this).parent('label.option').addClass('active-label');
+    }
+  });
+});
